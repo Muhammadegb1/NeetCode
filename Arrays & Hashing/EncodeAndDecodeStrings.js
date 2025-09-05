8dfaa73c7b411766918c4421517bb4002e2a6b2a
Muhammad_Egb <mhmad.m.ig2001@gmail.com>
2025-09-05 10:44:45 +0300

    Encode and Decode Strings

---
+++ Arrays & Hashing/EncodeAndDecodeStrings.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let wordEncoded = '';
+        for (const word of strs){
+            wordEncoded += word.length + "#" + word; 
+        } 
+        return wordEncoded;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let arr = [];
+        let i =0;
+        while (i < str.length){
+            let j = i;
+            while (str[j] !== '#'){
+                j++;
+            }
+            let length =parseInt(str.substring(i,j));
+            i = j + 1;
+            j = i + length;
+            arr.push(str.substring(i,j))
+            i=j;
+        }
+        return arr;
+    }
+}
+
+const sol = new Solution();
+let s = sol.encode(["neet","code","love","you"])
+console.log("encode: ", s);
+console.log("decode: ", sol.decode(s));
