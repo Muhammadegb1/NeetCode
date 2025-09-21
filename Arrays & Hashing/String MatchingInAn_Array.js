ebf5596e7b5216875c2077a57159c78816d2a78c
Muhammad_Egb <mhmad.m.ig2001@gmail.com>
2025-09-21 14:53:46 +0300

    String Matching in an Array

---
+++ Arrays & Hashing/String MatchingInAn_Array.js
@@ -0,0 +1,20 @@
+// String Matching in an Array
+
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var stringMatching = function(words) {
+    let n = words.length;
+    const result = [];
+
+    for (let i = 0; i < n ; i ++){
+        for (let j = 0; j < n; j++){
+            if (i !== j && words[j].includes(words[i])){
+                result.push(words[i]);
+                break;
+            }
+        }
+    }
+    return result;
+};
