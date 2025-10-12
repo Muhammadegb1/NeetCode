f2f586a62c40056e863ad8fff115c74d0fe2c30c
Muhammad_Egb <mhmad.m.ig2001@gmail.com>
2025-10-12 19:36:36 +0300

    Next Greater Element I

---
+++ Arrays & Hashing/NextGreaterElement.js
@@ -0,0 +1,26 @@
+//Next Greater Element I
+
+
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    nextGreaterElement(nums1, nums2) {
+        const stack = [];
+        const nextGreater = new Map();
+
+        for (let i = nums2.length - 1; i >= 0; i--) {
+            const current = nums2[i];
+            while (stack.length > 0 && stack[stack.length - 1] <= current) {
+                stack.pop();
+            }
+
+            nextGreater.set(num, stack.length === 0 ? -1 : stack[stack.length - 1]);
+            stack.push(num);
+        }
+
+        return nums1.map(num => nextGreater.get(num));
+    }
+}
